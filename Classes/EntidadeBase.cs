497aa04b22ffbac220624fe1e27d2ff1859da01b
DaniloAvelar <da.avelar@hotmail.com>
2021-04-29 11:23:58 -0300

    CRUD_APP - Simples para Series de TV, Contendo seus Generos

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace APP_CRUD
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace APP_CRUD
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos da Serie
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido {get; set;}
+
+        // Métodos ================================
+
+        // Construtor
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        // ToString :: Converte em string o retorno do objeto para ser impresso em tela
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        //Encapsulamento para retorno de Titulo e Id da Série, uma vez que os atributos são **Private**
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+    }
+}
